b52c7c4bbdb4b0b7509d3a655abc1941d5daea6c
ferreira-tb <109777783+ferreira-tb@users.noreply.github.com>
2022-12-03 14:23:53 -0300

    Corrigido problema com a exportação dos tipos

--- src/main.ts
+++ src/main.ts
@@ -7,3 +7,5 @@ export * from './endpoints/partidos.js';
 export * from './endpoints/proposicoes.js';
 export * from './endpoints/orgaos.js';
 export * from './endpoints/votacoes.js';
+
+export * from '../index.js';
